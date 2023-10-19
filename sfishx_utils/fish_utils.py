2413829a02488cbb7c092e19bd63df02a10a29fa
anferrar <anferrar@pop-os.localdomain>
2023-10-19 23:22:35 +0200

    Large update
    Note necessary anymore to pass latex labels, param names or fiducial,
    the code reads everything from the fisher and also creates by itself
    its latex labels. Also added the file to test the utilities, the file
    is not updated yet but it's being committed anyway.

--- sfishx_utils/fish_utils.py
+++ sfishx_utils/fish_utils.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import numpy as np
 from getdist import plots, loadMCSamples
 from getdist.gaussian_mixtures import GaussianND
@@ -23,25 +23,21 @@ class Fish_utils:
         self, 
         folder,
         model,
-        fiducial,
-        param_labels,
         steph_model = None,
         stephane = False,
         steph_tau = False
     ):
 
         self.model = model
-        self.fiducial = fiducial
-        self.param_labels = param_labels
 
         self.stephane = stephane
         self.steph_tau = steph_tau
         self.steph_model = steph_model
 
         if self.stephane:
-            self.fish_folder = '/media/veracrypt1/work_archives/archives/Code/CMBXC/forecast/cmbx_forecasts_outputs/fishers/Stephane'
+            self.fish_folder = '/media/veracrypt1/work_archives/cmbx_forecasts_outputs/fishers/Stephane'
             if steph_tau:
-                self.fish_folder_euclid_alone = '/media/veracrypt1/work_archives/archives/Code/CMBXC/forecast/cmbx_forecasts_outputs/fishers/Stephane/Euclid_with_tau'
+                self.fish_folder_euclid_alone = '/media/veracrypt1/work_archives/cmbx_forecasts_outputs/fishers/Stephane/Euclid_with_tau'
             else:
                 self.fish_folder_euclid_alone = self.fish_folder
         else:
@@ -49,19 +45,19 @@ class Fish_utils:
             self.fish_folder = folder
     
     
-    def get_E(self, spec, npt):
+    def get_E(self, spec, npt, dr1fsky):
         """ get Euclid alone fisher matrices
 
             Args:
                 spec (string): Euclid specifications (opti or pess)
         """
 
-        cosmo_model = set_model(self.model, self.fiducial, self.param_labels)
+        cosmo_model = set_model(self.model)
 
-        return euclid_alone(self.fish_folder_euclid_alone, self.stephane, self.steph_model, spec, cosmo_model, npt)
+        return euclid_alone(self.fish_folder_euclid_alone, self.stephane, self.steph_model, spec, cosmo_model, npt, dr1fsky)
 
 
-    def get_Ecmb(self, spec, cmb, mode, npt, probes=None):
+    def get_Ecmb(self, spec, cmb, mode, npt, dr1fsky, pr4, probes=None):
         """ get Euclid combined with CMB fisher matrices
 
             Args:
@@ -77,8 +73,8 @@ class Fish_utils:
                                                    consider only CMBlensing
         """
 
-        cosmo_model = set_model(self.model, self.fiducial, self.param_labels)
-        return euclid_cmb(self.fish_folder, self.stephane, self.steph_model, spec, cmb, mode, cosmo_model, npt, probes)
+        cosmo_model = set_model(self.model)
+        return euclid_cmb(self.fish_folder, self.stephane, self.steph_model, spec, cmb, mode, cosmo_model, npt, dr1fsky, pr4, probes)
 
     def get_cmb(self, cmb, npt, probes=None):
         """ get CMB alone fisher matrices
@@ -91,62 +87,39 @@ class Fish_utils:
                                                    consider only CMB primaries(TTTEEE)
         """
 
-        cosmo_model = set_model(self.model, self.fiducial, self.param_labels)
+        cosmo_model = set_model(self.model)
         return cmb_alone(self.fish_folder, self.stephane, self.steph_model, cmb, cosmo_model, npt, probes)
 
 
-def set_model(model, fiducial, param_labels):
-    model_dict = {'model' : model,
-                  'fiducial' : fiducial,
-                  'param_labels' : param_labels[:],
-                }
-    if model == 'LCDM':
-        model_dict['model_tex'] = r'$\Lambda \rm CDM$'
-    if model == 'IG':
-        model_dict['model_tex'] = r'$\rm IG$'
-    if model == 'NMCxpDelta':
-        model_dict['model_tex'] = r'${\rm NMC}+ (\xi, \Delta)$'
+def set_model(model):
+    model_dict = {'model' : model}
 
-    model_dict['param_names'] = [par for par in model_dict['fiducial']],
-    model_dict['means'] = [model_dict['fiducial'][par] for par in model_dict['fiducial']],
+    if model.lower() == 'lcdm':
+        model_dict['model_tex'] = r'$\Lambda \rm CDM$'
+    if model.lower() == 'ig':
+        model_dict['model_tex'] = r'$\rm IG$'
+    if model.lower() == 'nmcxpdelta':
+        model_dict['model_tex'] = r'${\rm NMC}+ (\xi, \Delta)$'
     
     return model_dict
 
-def euclid_alone(fish_folder, stephane, steph_model, spec, model, npt):
-
-    Euclid_alone = {
-        'spec' : spec,
-        'model' : model['model'],
-        'label' : '3x2pt Euclid ' + spec,
-        'model_tex': model['model_tex']
-    }
+def euclid_alone(fish_folder, stephane, steph_model, spec, model, npt, dr1fsky):
 
     if stephane:
-        Euclid_alone['file'] = os.path.join(fish_folder, "fish_Euclid-" + spec + "_" + steph_model + "_" + "max-bins_super-prec_21point.npz")
+        # Euclid_alone['file'] = os.path.join(fish_folder, "fish_Euclid-" + spec + "_" + steph_model + "_" + "max-bins_super-prec_21point.npz")
+        filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_" + steph_model + "_" + "max-bins_super-prec_21point.npz")
     else:
-        Euclid_alone['file'] = os.path.join(fish_folder, "fish_Euclid-" + spec + "_flat_" + "max-bins_super-prec_" + npt + ".npz")
-
-    fish = np.load(Euclid_alone['file'])
-    Euclid_alone['fish_file'] = fish
-    Euclid_alone['fish'] = fish['fish']
-    Euclid_alone['cov'] = fish['cov']
-    Euclid_alone['me'] = fish['me']
-    Euclid_alone['wid'] = fish['wid']
-    Euclid_alone['prec'] = fish['prec']
-    Euclid_alone['param_names'] = model['param_names'][0]
-    Euclid_alone['fiducial'] = model['fiducial']
+        if dr1fsky:
+            filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_flat_" + "max-bins_super-prec_dr1_fsky_" + npt + ".npz")
+        else:
+            filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_flat_" + "max-bins_super-prec_" + npt + ".npz")
     
-    FMsample = GaussianND(model['means'][0], Euclid_alone['cov'],
-                          #is_inv_cov=True,
-                          names = model['param_names'][0],
-                          labels = model['param_labels']
-                          )
-    Euclid_alone['FMsample'] = FMsample
-
+    Euclid_alone = build_dict(filename, model, spec)
+    Euclid_alone['label'] = '3x2pt Euclid ' + spec,
 
     return Euclid_alone
 
-def euclid_cmb(fish_folder, stephane, steph_model, spec, cmb, mode, model, npt, probes = None):
+def euclid_cmb(fish_folder, stephane, steph_model, spec, cmb, mode, model, npt, dr1fsky, pr4, probes = None):
     
     """
      mode means the way we combine the probes
@@ -155,94 +128,191 @@ def euclid_cmb(fish_folder, stephane, steph_model, spec, cmb, mode, model, npt, 
                                          * c ("cross") == full CMBxEucld covariance and data
     """
 
-    Euclid_cmb = {
-        'spec' : spec,
-        'model' : model['model'],
-        'cmb' : cmb,
-        'mode' : mode,
-        'model_tex': model['model_tex']
-    }
-
     if stephane:
         if probes == 'phionly':
-            Euclid_cmb['file'] = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMBphionly-" + cmb + "_mode-" + mode + "_" + steph_model + "_max-bins_super-prec_21point.npz")
+            filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMBphionly-" + cmb + "_mode-" + mode + "_" + steph_model + "_max-bins_super-prec_21point.npz")
         else:
-            Euclid_cmb['file'] = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMB-" + cmb + "_mode-" + mode + "_" + steph_model + "_max-bins_super-prec_" + npt + ".npz")
+            filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMB-" + cmb + "_mode-" + mode + "_" + steph_model + "_max-bins_super-prec_" + npt + ".npz")
     else:
         if probes == 'phionly':
-            Euclid_cmb['file'] = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMBphionly-" + cmb + "_mode-" + mode + "_flat_max-bins_super-prec_" + npt + ".npz")
+            if (dr1fsky):
+                if pr4:
+                    filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMBphionly-" + cmb + "PR4_mode-" + mode + "_flat_max-bins_super-prec_dr1_fsky_" + npt + ".npz")
+                else:
+                    filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMBphionly-" + cmb + "_mode-" + mode + "_flat_max-bins_super-prec_dr1_fsky_" + npt + ".npz")
+            else:
+                if pr4:
+                    filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMBphionly-" + cmb + "PR4_mode-" + mode + "_flat_max-bins_super-prec_" + npt + ".npz")
+                else:
+                    filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMBphionly-" + cmb + "_mode-" + mode + "_flat_max-bins_super-prec_" + npt + ".npz")
         else:
-            Euclid_cmb['file'] = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMB-" + cmb + "_mode-" + mode + "_flat_max-bins_super-prec_" + npt + ".npz")
+            filename = os.path.join(fish_folder, "fish_Euclid-" + spec + "_CMB-" + cmb + "_mode-" + mode + "_flat_max-bins_super-prec_" + npt + ".npz")
 
     if probes == 'phionly':
         if mode == 'p' or mode =='a':
-            Euclid_cmb['label']= '3x2pt Euclid ' + spec + ' + CMBlens ' + cmb + '-like'
+            label = '3x2pt Euclid ' + spec + ' + CMBlens ' + cmb + '-like'
         else:
-            Euclid_cmb['label']= '6x2pt Euclid ' + spec + ' ' + cmb + '-like'
+            label = '6x2pt Euclid ' + spec + ' ' + cmb + '-like'
     else:
         if mode == 'p' or mode =='a':
-            Euclid_cmb['label']= '3x2pt Euclid ' + spec + ' + CMB ' + cmb + '-like'
+            label = '3x2pt Euclid ' + spec + ' + CMB ' + cmb + '-like'
         else:
-            Euclid_cmb['label']= '3x2pt Euclid ' + spec + ' +X CMB ' + cmb + '-like'
-    
-    fish = np.load(Euclid_cmb['file'])
-    Euclid_cmb['fish_file'] = fish
-    Euclid_cmb['fish'] = fish['fish']
-    Euclid_cmb['cov'] = fish['cov']
-    Euclid_cmb['me'] = fish['me']
-    Euclid_cmb['wid'] = fish['wid']
-    Euclid_cmb['prec'] = fish['prec']
-    Euclid_cmb['param_names'] = model['param_names'][0]
-    Euclid_cmb['fiducial'] = model['fiducial']
-    
-    FMsample = GaussianND(model['means'][0], Euclid_cmb['cov'],
-                          #is_inv_cov=True,
-                          names = model['param_names'][0],
-                          labels = model['param_labels']
-                          )
-    Euclid_cmb['FMsample'] = FMsample
-   
+            label = '3x2pt Euclid ' + spec + ' +X CMB ' + cmb + '-like'
 
+    Euclid_cmb = build_dict(filename, model, spec)
+    Euclid_cmb['label'] = label
+    Euclid_cmb['cmb'] = cmb
+    Euclid_cmb['mode'] : mode
+    
     return Euclid_cmb
 
 
 def cmb_alone(fish_folder, stephane, steph_model, cmb, model, npt, probes):
 
-    cmb_alone = {
-        'cmb' : cmb,
-        'model' : model['model'],
-        'label' : 'CMB ' + cmb + '-like',
-        'model_tex': model['model_tex']
-    }
-    
+    label = 'CMB ' + cmb + '-like',
+
     if probes == 'primonly':
-        cmb_alone['label'] = "TTTEEE " + cmb + '-like'
+        label = "TTTEEE " + cmb + '-like'
         if stephane:
-            cmb_alone['file'] = os.path.join(fish_folder, "fish_CMB-" + probes + "-" + cmb + "_" + steph_model + "_max-bins_super-prec_21point.npz")
+            filename = os.path.join(fish_folder, "fish_CMB-" + probes + "-" + cmb + "_" + steph_model + "_max-bins_super-prec_21point.npz")
         else:
-            cmb_alone['file'] = os.path.join(fish_folder, "fish_CMB-" + cmb + "_flat_max-bins_super-prec_" + npt + ".npz")
+            filename = os.path.join(fish_folder, "fish_CMB-" + cmb + "_flat_max-bins_super-prec_" + npt + ".npz")
     else:
         if stephane:
-            cmb_alone['file'] = os.path.join(fish_folder, "fish_CMB-" + cmb + "_" + steph_model + "_max-bins_super-prec_" + npt + ".npz")
+            filename = os.path.join(fish_folder, "fish_CMB-" + cmb + "_" + steph_model + "_max-bins_super-prec_" + npt + ".npz")
         else:
-            cmb_alone['file'] = os.path.join(fish_folder, "fish_CMB-" + cmb + "_flat_max-bins_super-prec_" + npt + ".npz")
-    
-    fish = np.load(cmb_alone['file'])
-    cmb_alone['fish_file'] = fish
-    cmb_alone['fish'] = fish['fish']
-    cmb_alone['cov'] = fish['cov']
-    cmb_alone['me'] = fish['me']
-    cmb_alone['wid'] = fish['wid']
-    cmb_alone['prec'] = fish['prec']
-    cmb_alone['param_names'] = model['param_names'][0]
-    cmb_alone['fiducial'] = model['fiducial']
-    
-    FMsample = GaussianND(model['means'][0], cmb_alone['cov'],
-                          #is_inv_cov=True,
-                          names = model['param_names'][0],
-                          labels = model['param_labels']
-                          )
-    cmb_alone['FMsample'] = FMsample
+            filename = os.path.join(fish_folder, "fish_CMB-" + cmb + "_flat_max-bins_super-prec_" + npt + ".npz")
 
-
+    cmb_alone = build_dict(filename, model)
+    cmb_alone['label'] = label
+    cmb_alone['cmb'] = cmb
+    
     return cmb_alone
+
+
+def build_dict(filename, model, spec = None):
+    fish_dict = {
+        'spec' : spec,
+        'model' : model['model'],
+        'model_tex': model['model_tex']
+    }
+
+    fish = np.load(filename)
+
+    fish_dict['fish_file'] = fish
+    fish_dict['fish'] = fish['fish']
+    fish_dict['cov'] = fish.get('cov')
+    fish_dict['me'] = fish['me']
+    fish_dict['wid'] = fish['wid']
+    fish_dict['prec'] = fish['prec']
+    fish_dict['param_names'] = fish['all_params']
+    fish_dict['param_names'] = ['om', 'ob', 'h', 'ns', 'sigma8', 'tau']
+
+
+    param_labels = latex_pnames(fish_dict['param_names'])
+
+    fish_dict['param_labels'] = param_labels
+
+    fish_dict['fiducial'] = {}
+    for i, p in enumerate(fish_dict['param_names']):
+        fish_dict['fiducial'][p] = fish_dict['me'][i]
+
+    if len(param_labels) != len(fish_dict['param_names']):
+        print("Error, the lenght of param_labels is different form the length of param names\n"
+              "You probably passed a fisher with a parameter not contemplated by the function latex_pnames")
+        sys.exit()
+    
+    FMsample = GaussianND(fish_dict['me'], fish_dict['fish'],
+                          is_inv_cov=True,
+                          names = fish_dict['param_names'],
+                          # labels = model['param_labels']
+                          labels = param_labels
+                          )
+    fish_dict['FMsample'] = FMsample
+
+
+    return fish_dict
+
+
+def latex_pnames(pnames):
+    latex_list = []
+    for p in pnames:
+        if (p == 'ob') or (p == b'ob'):
+            latex_list.append(r"\Omega_{\rm b}")
+
+        if (p == 'om') or (p == b'om'):
+            latex_list.append(r"\Omega_{\rm m}")
+
+        if (p == 'sigma8') or (p == b'sigma8'):
+            latex_list.append(r"\sigma_8")
+
+        if (p == 'tau') or (p == b'tau'):
+            latex_list.append(r"\tau")
+            
+        if (p == 'ns') or (p == b'ns'):
+            latex_list.append(r"n_{\rm s}")
+
+        if (p == 'h') or (p == b'h'):
+            latex_list.append(r"h")
+
+        if (p == 'delta_IG') or (p == 'Delta'):
+            latex_list.append(r"\Delta")
+
+        if p == 'gamma_IG':
+            latex_list.append(r"\xi")
+
+        if p == 'mnu':
+            latex_list.append(r"m_{\nu}")
+
+        if p == 'aIA':
+            latex_list.append(r"a_{\rm IA}")
+
+        if p == 'eIA':
+            latex_list.append(r"e_{\rm IA}")
+
+        if p == 'bIA':
+            latex_list.append(r"b_{\rm IA}")
+
+        if p == 'b0':
+            latex_list.append(r"b_{0}")
+
+        if p == 'b1':
+            latex_list.append(r"b_{1}")
+
+        if p == 'b2':
+            latex_list.append(r"b_{2}")
+
+        if p == 'b3':
+            latex_list.append(r"b_{3}")
+
+        if p == 'b4':
+            latex_list.append(r"b_{4}")
+
+        if p == 'b5':
+            latex_list.append(r"b_{5}")
+
+        if p == 'b6':
+            latex_list.append(r"b_{6}")
+
+        if p == 'b7':
+            latex_list.append(r"b_{7}")
+
+        if p == 'b8':
+            latex_list.append(r"b_{8}")
+
+        if p == 'b9':
+            latex_list.append(r"b_{9}")
+
+        if p == 'b10':
+            latex_list.append(r"b_{10}")
+
+        if p == 'b11':
+            latex_list.append(r"b_{11}")
+
+        if p == 'b12':
+            latex_list.append(r"b_{12}")
+
+        if p == 'b13':
+            latex_list.append(r"b_{13}")
+
+    return latex_list
